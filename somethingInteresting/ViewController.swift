d663dfa8ced7284d87113f8b9b6ce4f3a0b1fffe
Donavin Wats <donnist@gmail.com>
2020-05-01 20:48:25 -0400

    Initial Commit

---
+++ somethingInteresting/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  somethingInteresting
+//
+//  Created by Donavin Watson on 5/1/20.
+//  Copyright © 2020 Don Watsy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
