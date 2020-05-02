f5de7f6df9e05e187d8a2c414a5ce9f19402e1c4
Donavin Wats <donnist@gmail.com>
2020-05-02 09:42:10 -0400

    Updated

--- somethingInteresting/ViewController.swift
+++ somethingInteresting/ViewController.swift
@@ -16,54 +16,41 @@ class ViewController: UIViewController {
 	
 	
 	var moonPhases: Array = [
-		["fullMoon_1.png", "Full Moon"] ,
-		["waxGibbous_2.png", "Waxing Gibbous"],
-		["firstQuarter_3.png", "First Quarter"],
-		["waxCrescent_4.png", "Waxing Crescent"],
-		["newMoon_5.png", "New Moon"],
-		["wanCrescent_6.png", "Waning Crescent"],
+		["fullMoon_1.png",     "Full Moon"],
+		["wanGibbous_8.png",   "Waning Gibbous"],
 		["thirdQuarter_7.png", "Third Quarter"],
-		["wanGibbous_8.png", "Waning Gibbous"]
+		["wanCrescent_6.png",  "Waning Crescent"],
+		["newMoon_5.png",      "New Moon"],
+		["waxCrescent_4.png",  "Waxing Crescent"],
+		["firstQuarter_3.png", "First Quarter"],
+		["waxGibbous_2.png",   "Waxing Gibbous"]
 	];
+	
+	var index: Int = 0 {
+		didSet {
+			updateUI()
+		}
+	}
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
-		
-		UpdateUI()
-		setNeedsStatusBarAppearanceUpdate();
-		
+		index = 0; //Start with full moon.
 	}
-	
-	
-	var Index: Int = 0;
-	
 	
 	@IBAction func slideChangeImage(_ sender: UISlider) {
 		
-		let value = Int(sender.value)
-		imageView.image = UIImage(named: moonPhases[value][0]);
-		imageLabel.text = moonPhases[value][1];
-		for value in 0..<moonPhases.count {
-			
-			print(value)
-			
-		}
+		index = Int(sender.value)
 	}
 	
-	
-	func UpdateUI(){
-		imageView.image = UIImage(named: moonPhases[0][0]);
-		imageLabel.text = moonPhases[0][1];
-		
-		setNeedsStatusBarAppearanceUpdate();
-		
+	func updateUI() {
+		imageView.image = UIImage(named: moonPhases[index][0]);
+		imageLabel.text = moonPhases[index][1];
 	}
+	
 	override var preferredStatusBarStyle: UIStatusBarStyle {
-	return .lightContent;
+		return .lightContent;
+		
 	}
+	
 }
-
-
-
-
